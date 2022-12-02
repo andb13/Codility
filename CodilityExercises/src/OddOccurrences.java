8d51ad794edc47729ce131c986e0fdb22c17e481
aibi <andrei.buhai@gft.com>
2022-12-02 12:25:28 +0100

    Changed without boolean array

--- CodilityExercises/src/OddOccurrences.java
+++ CodilityExercises/src/OddOccurrences.java
@@ -3,17 +3,11 @@ import java.util.Arrays;
 public class OddOccurrences {
     public static int solution(int[] A) {
         int length = A.length;
-        boolean visited[] = new boolean[length];
-        Arrays.fill(visited, false);
         int nonOccurNum = 0;
-        for(int i=0; i<length; i++){
-            if(visited[i]) 
-                continue;
-            
-            int count = 1;
-            for(int j = i+1; j<length;j++){
-                if(A[i] == A[j]){
-                    visited[j] = true;
+        for(int i=0; i<length; i++){   
+            int count = 0;        
+            for(int j=0; j<length;j++){
+                if(A[i]==A[j]){
                     count++;
                 }
             }
