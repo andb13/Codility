9a325c2e8c65dfc94e51db1b0f82e903a1265884
aibi <andrei.buhai@gft.com>
2022-12-16 10:16:11 +0100

    refactored Distinct to use set.size()

--- CodilityExercises/src/Distinct.java
+++ CodilityExercises/src/Distinct.java
@@ -7,17 +7,13 @@ public class Distinct {
     public static int solution(int[] A){
         
         Set<Integer> distinctNumbers = new HashSet<>();
-        int distinctValues = 0;
 
         for (int num : A) {
             distinctNumbers.add(num);
         }
-
-        for (int num : distinctNumbers) {
-            distinctValues++;
-        }
         
-        return distinctValues;
+        return distinctNumbers.size();
+        
     }
 
 }
